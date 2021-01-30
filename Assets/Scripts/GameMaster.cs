649843d0bd2ce3e6a1510868aaf53aa1875eedaa
skypekitten9 <victor.enhorning@gmail.com>
2021-01-30 09:53:03 +0100

    Added GameManager and a Goal. Game can be reset with R and next level can be entered with T.

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour
+{
+    private static GameMaster instance = null;
+    public static GameMaster Instance { get { return instance; } }
+
+    private int sceneIndex = 0;
+
+    public void Awake()
+    {
+        if (instance != null && instance != this)
+            Destroy(this.gameObject);
+        else
+            instance = this;
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            ResetLevel();
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            NextLevel();
+        }
+    }
+
+    public void ResetLevel()
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void NextLevel()
+    {
+        sceneIndex++;
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    bool transitioning = false;
+    bool goalReached = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        goalReached = true;
+    }
+
+    private void Update()
+    {
+        if (goalReached && !transitioning)
+        {
+            transitioning = true;
+            GameMaster.Instance.NextLevel();
+        }
+    }
+}
