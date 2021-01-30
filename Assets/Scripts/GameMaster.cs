4b05f47e2e57cb8d84b86dd030cc7c1cbe477edd
skypekitten9 <victor.enhorning@gmail.com>
2021-01-30 10:34:32 +0100

    Added win/loose and decrease life in game manager.

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -7,8 +7,10 @@ public class GameMaster : MonoBehaviour
 {
     private static GameMaster instance = null;
     public static GameMaster Instance { get { return instance; } }
+    public int life;
 
-    private int sceneIndex = 0;
+    int sceneIndex = 0;
+
 
     public void Awake()
     {
@@ -30,16 +32,41 @@ public class GameMaster : MonoBehaviour
         {
             NextLevel();
         }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            DecreaseLife();
+        }
+    }
+
+    public void Win()
+    {
+        NextLevel();
+    }
+
+    public void DecreaseLife()
+    {
+        life--;
+        if(life <= 0)
+        {
+            Loose();
+        }
+    }
+
+    public void Loose()
+    {
+        ResetLevel();
     }
 
     public void ResetLevel()
     {
+        life = 3;
         SceneManager.LoadScene(sceneIndex);
     }
 
     public void NextLevel()
     {
         sceneIndex++;
+        life = 3;
         SceneManager.LoadScene(sceneIndex);
     }
 }
