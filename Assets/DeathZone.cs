57d748d1dfbf9be975fca7164c281f65f3d35dfa
skypekitten9 <victor.enhorning@gmail.com>
2021-01-31 15:29:30 +0100

    Level 1 + ui fix.

---
+++ Assets/DeathZone.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    bool transitioning = false;
+    bool deathZoneReached = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player") deathZoneReached = true;
+    }
+
+    private void Update()
+    {
+        if (deathZoneReached && !transitioning)
+        {
+            Debug.Log("Hewo");
+            transitioning = true;
+            GameMaster.Instance.Loose();
+        }
+    }
+}
